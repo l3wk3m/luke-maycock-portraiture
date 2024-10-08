07f91048e417965d7b225c56ea571eced856cb2e
Luke Maycock <maycock.luke@gmail.com>
2024-10-08 23:46:26 +0000

    Fix: Commented out currently useless features in the profiles view to allow a basic page render

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def profile(request):
     """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    # profile = get_object_or_404(UserProfile, user=request.user)
     
-    orders = profile.orders.all()
+    # orders = profile.orders.all()
 
     template = 'profiles/profile.html'
 
-    context = {
-        'orders': orders,
-    }
+    # context = {
+    #     'orders': orders,
+    # }
 
-    return render(request, template, context)
+    return render(request, template)
