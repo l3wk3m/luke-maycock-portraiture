4bf2ee8845fe5e5a4690ea01fd6825806173028a
Luke Maycock <maycock.luke@gmail.com>
2024-10-13 01:02:19 +0000

    Feat: Added the 'comments' custom model

--- luke_maycock_portraiture/settings.py
+++ luke_maycock_portraiture/settings.py
@@ -113,6 +113,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+ #     'default': {
+ #         'ENGINE': 'django.db.backends.sqlite3',
+ #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #     }
+ # }
+     
+# DATABASES = {
+#     'default': dj_database_url.parse('sql url')
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- products/models.py
+++ products/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+
+from profiles.models import UserProfile
 
 # Create your models here.
 
@@ -28,6 +31,15 @@ class Product(models.Model):
     stock = models.IntegerField(default=3)
     image_url = models.URLField(max_length=1000, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Comments(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='comments')
+    title = models.CharField(max_length=200)
+    comment = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
