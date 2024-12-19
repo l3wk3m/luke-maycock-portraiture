a6f8e8a426772a55cd6bfba33a580f42c1b93ea5
Luke Maycock <maycock.luke@gmail.com>
2024-12-19 08:36:30 +0000

    Fix: Added foreign key to Order model to link to username in profiles model

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
-# Create your views here.
+
 def profile(request):
     """ Display the user's profile. """
-    # profile = get_object_or_404(UserProfile, user=request.user)
-    
-    # orders = profile.orders.all()
 
     template = 'profiles/profile.html'
+    context = {}
 
-    # context = {
-    #     'orders': orders,
-    # }
-
-    return render(request, template)
+    return render(request, template, context)
