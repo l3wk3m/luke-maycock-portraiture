c3825ca8a7f7512fc3040f45f6cd3a767d7613f3
Luke Maycock <maycock.luke@gmail.com>
2024-12-19 08:23:38 +0000

    Updated order model with django countries

--- luke_maycock_portraiture/settings.py
+++ luke_maycock_portraiture/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-l3wk3m-lukemaycockportr-vrfr9hi7nc8.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-l3wk3m-lukemaycockportr-y6hiff27w5o.ws.codeinstitute-ide.net']
 
 
 # Application definition
